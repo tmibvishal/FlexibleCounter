4c76ee9bab43ef2b437ceff2167a5d777a086139
Vishal Singh <tmibvishal@gmail.com>
2018-12-26 19:15:17 +0530

    set adapter to recyler view

--- app/src/main/java/tmib/vishal/flexiblecounter/MainActivity.java
+++ app/src/main/java/tmib/vishal/flexiblecounter/MainActivity.java
@@ -1,13 +1,32 @@
 package tmib.vishal.flexiblecounter;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import tmib.vishal.flexiblecounter.adapters.customAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView mRecyclerView;
+    RecyclerView.LayoutManager mLayoutManager;
+    RecyclerView.Adapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recylerView);
+        // used to increase performance in case the layout size does not changes
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mAdapter = new customAdapter();
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
